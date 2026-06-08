333c14d438c596404aaae6e908ef24c35458712f
ESK-reva <eshansk1457@gmail.com>
2026-06-08 13:02:18 +0530

    fix: add missing function prototypes for initializeCanvas and displayCanvas

--- main.c
+++ main.c
@@ -14,3 +14,15 @@ void initializeCanvas()
     }
 }
 }
+void displayCanvas()
+{   
+for(i=0;i<ROWS;i++)
+{
+    for(j=0;j<COLS;j++)
+    {
+        printf("%c",canvas[i][j]);
+    }
+
+    printf("\n");
+}
+}
